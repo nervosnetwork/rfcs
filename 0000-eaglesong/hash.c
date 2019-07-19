c055c329f7e2aec6325226314ffa41048643c020
Alan Szepieniec <alan@nervos.org>
2019-07-19 18:28:29 +0200

    Eaglesong RFC, along with attachments (reference implementations, code for generating constants)

---
+++ 0000-eaglesong/hash.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void EaglesongHash( unsigned char * output, const unsigned char * input, int input_length );
+
+void main( int argc, char ** argv ) {
+    unsigned char input[10000];
+    unsigned char output[32];
+    int c;
+    int i;
+
+    i = 0;
+    while( 1 ) {
+	    c = getchar();
+	    if( c == EOF ) {
+            break;
+        }
+        input[i] = c;
+        i = i + 1;
+    }
+
+    EaglesongHash(output, input, i);
+
+    for( i = 0 ; i < 32 ; ++i ) {
+        printf("%02x", output[i]);
+    }
+
+    printf("\n");
+}
